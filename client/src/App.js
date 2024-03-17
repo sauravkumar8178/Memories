e3ef07959bea250caa9e2d347cfb2f275cdb94d2
Saurav Kumar <ss989108@gmail.com>
2024-03-17 22:19:10 +0530

    first phase

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import { Container ,AppBar, Typography, Grow, Grid } from '@mui/material';
+
+import memories from './images/memories.jpeg';  
+
 const App = () => {
     return (
-        <div>
-            <h1>Memories</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memorise" height="60" />
+            </AppBar>
+        </Container>
     );
 }
 
