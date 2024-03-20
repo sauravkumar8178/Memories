012e2ee01744caaf75b91ef35fcd19be6b5b4578
Saurav Kumar <ss989108@gmail.com>
2024-03-20 07:14:56 +0530

    improving ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,32 @@ import React from "react";
 
 import { Container ,AppBar, Typography, Grow, Grid } from '@mui/material';
 
-import memories from './images/memories.jpeg';  
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form"
+import memories from './images/memories.png';  
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memorise" height="60" />
+            <AppBar position="static" color="inherit" className={classes.AppBar}>
+                <Typography variant="h2" align="center" className={classes.heading}>Memories</Typography>
+                <img src={memories} alt="memorise" height="60" className={classes.image} />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
